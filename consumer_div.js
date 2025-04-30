a9be3160cbe33908903ce9863ed9bde6121d155a
Vouts <machevoutsa@gmail.com>
2025-04-30 10:12:48 +0200

    Ajout d'exchange sur le consumer div

--- consumer_div.js
+++ consumer_div.js
@@ -1,33 +1,37 @@
 const amqp = require('amqplib');
 
+const queue = 'calc_requests';
+const resultQueue = 'calc_results';
+const exchange = 'calc_exchange';
+let channel;
+
 async function startWorker() {
     const conn = await amqp.connect('amqp://localhost'); 
-    const ch = await conn.createChannel();
+    channel= await conn.createChannel();
 
-    const queue = 'calc_requests';
-    await ch.assertQueue(queue, { durable: false });
+    await channel.assertExchange(exchange, 'direct', { durable: false });
 
-    const resultQueue = 'calc_results';
-    await ch.assertQueue(resultQueue, { durable: false });
+    await channel.assertQueue(queue, { durable: false });
+    
+    await channel.assertQueue(resultQueue, { durable: false });
 
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-    ch.consume(queue, async (msg) => {
+    channel.consume(queue, async (msg) => {
         const { n1, n2, op } = JSON.parse(msg.content.toString());
         if (op === 'div') {
             const result = n1 / n2;
-          //  console.log(` Performing addition: ${n1} + ${n2} = ${result}`);
 
             const delay = Math.floor(Math.random() * 10000) + 5000;
             setTimeout(() => {
                 const resultMsg = JSON.stringify({
                     n1, n2, op, result
                 });
-                ch.sendToQueue(resultQueue, Buffer.from(resultMsg));
+                channel.sendToQueue(resultQueue, Buffer.from(resultMsg));
                 console.log(`  Sent result: ${resultMsg}`);
             }, delay);
         }
-        ch.ack(msg);
+        channel.ack(msg);
     });
 }
 
